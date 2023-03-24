44ba0ab8778c52adc6ca3e83cd32916d11379d1a
Daniel Suen <75111446+kyddaniel@users.noreply.github.com>
2023-03-24 23:17:24 +0800

    Added pages and Navigation Bars. (Imports: BrowserRouter, Routes, Route, Link)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NavBar from './NavBar';
+
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlesListPage from './pages/ArticlesListPage';
+import ArticlePage from './pages/ArticlePage';
+
 function App() {
   return (
-    <div className="App">
-      <h1>My Blog</h1>
-      <div id="page-body">
-        Welcome to my blog!
+    <BrowserRouter>
+      <div className="App">
+        <NavBar></NavBar>
+        <div id="page-body">
+          <Routes>
+            <Route path="/"                    element={<HomePage />} />
+            <Route path="/about"               element={<AboutPage />} />
+            <Route path="/articles"            element={<ArticlesListPage />} />
+            <Route path="/articles/:articleId" element={<ArticlePage />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/ArticlesListPage.js
@@ -0,0 +1,7 @@
+const ArticlesListPage = () => {
+    return (
+        <h1>This is the article list page!</h1>
+    );
+}
+
+export default ArticlesListPage;
